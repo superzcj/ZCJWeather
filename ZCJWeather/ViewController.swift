28fe88f32746f43a90e45850fe3e1b2f354e13c4
zcj <405911774@qq.com>
2016-06-21 10:08:27 +0800

    Initial Commit

---
+++ ZCJWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZCJWeather
+//
+//  Created by zhangchaojie on 16/6/21.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
